46baed01d74decd0a0f6dca3d7ae0ed64d35b61b
Andrew Skubarenko <andrew.skubarenko@gmail.com>
2024-06-28 09:37:25 +0300

    Update the loading env in the integration tests

--- integration/jest.config.js
+++ integration/jest.config.js
@@ -2,8 +2,8 @@
 const path = require('path');
 const dotenv = require('dotenv');
 
-dotenv.config({ path: path.resolve(__dirname, './.env'), override: true })
-dotenv.config({ path: path.resolve(__dirname, './.env.local'), override: true })
+dotenv.config({ path: path.resolve(__dirname, './.env.local') })
+dotenv.config({ path: path.resolve(__dirname, './.env') })
 
 /*
  * For a detailed explanation regarding each configuration property, visit:
