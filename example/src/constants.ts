037485c317e2af369584684ee1e8fee85d1622f1
jack.squirrel <t.ismagilov@longgammalabs.com>
2024-08-27 19:04:48 +0200

    New abi and contracts

--- example/src/constants.ts
+++ example/src/constants.ts
@@ -1,4 +1,4 @@
 export const RPC_URL = 'https://node.ghostnet.etherlink.com';
 export const HANJI_API_BASE_URL = 'https://api-dev.hanji.io';
 export const HANJI_WEBSOCKET_BASE_URL = 'wss://sockets-dev.hanji.io';
-export const MARKET_ADDRESS = '0x98Cb927F0896306377e3be11ee39EFbbDE98F274'.toLowerCase();
+export const MARKET_ADDRESS = '0x7bE35914aEDA4763025310aac687eFC8014d2F61'.toLowerCase();
