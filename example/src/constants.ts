0d2474da9972b768d8a62a62e34ba2e1be1476e9
Timur Ismagilov <150016726+jack-squirrel@users.noreply.github.com>
2024-09-19 22:02:23 +0200

    Updated: lob addresses (#18)

--- example/src/constants.ts
+++ example/src/constants.ts
@@ -1,4 +1,4 @@
 export const RPC_URL = 'https://node.ghostnet.etherlink.com';
 export const HANJI_API_BASE_URL = 'https://api-dev.hanji.io';
 export const HANJI_WEBSOCKET_BASE_URL = 'wss://sockets-dev.hanji.io';
-export const MARKET_ADDRESS = '0x69Fa40D083d59517F83D628Addde5DCf4fc4F50b'.toLowerCase();
+export const MARKET_ADDRESS = '0xaE8B460B6DdD4eE8CD38BEB524cf41f8f96E1fCd'.toLowerCase();
