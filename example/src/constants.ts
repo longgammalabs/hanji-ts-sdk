7bb86d899400acc7bb1f13e2fea4f64038201e24
Nikolay Strekalov <n.strekalov@longgammalabs.com>
2024-11-08 20:01:19 +0300

    feature: update testnet contracts

--- example/src/constants.ts
+++ example/src/constants.ts
@@ -1,4 +1,4 @@
 export const RPC_URL = 'https://node.ghostnet.etherlink.com';
 export const HANJI_API_BASE_URL = 'https://api-dev.hanji.io';
 export const HANJI_WEBSOCKET_BASE_URL = 'wss://sockets-dev.hanji.io';
-export const MARKET_ADDRESS = '0x3f295daf44fb09bcdf61d7727b8a202e3393f9be'.toLowerCase();
+export const MARKET_ADDRESS = '0xA86F0AD9b353BF0De47DC6F367fdEc669D92F6fB'.toLowerCase();
